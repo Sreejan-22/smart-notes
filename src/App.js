6ca4dc277edfb5652197eb6ef514fe345b546f1d
Sreejan <sreejan147@gmail.com>
2021-07-16 15:05:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { Switch, Route } from "react-router-dom";
-import Notes from "./pages/Notes";
-import Create from "./pages/Create";
+import React from "react";
+// import { Switch, Route } from "react-router-dom";
+// import Notes from "./pages/Notes";
+// import Create from "./pages/Create";
+import Checkout from "./Checkout";
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/create">
-        <Create />
-      </Route>
-      <Route path="/">
-        <Notes />
-      </Route>
-    </Switch>
+    // <Switch>
+    //   <Route path="/create">
+    //     <Create />
+    //   </Route>
+    //   <Route path="/">
+    //     <Notes />
+    //   </Route>
+    // </Switch>
+    <React.Fragment>
+      <Checkout />
+    </React.Fragment>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+// const publishableKey = await fetch("/config").then((r) => r.json());
+// const stripePromise = loadStripe(publishableKey);
+const stripePromise = loadStripe(
+  "pk_test_51IyX4qSBIOHRJF6e3kT1aaFAfvHhbPXS4o4Ee2EfRTRxP8z3ZAvMQVz8AzxfejGl5hTMhO5sQgAFkrFA66ZMTk6t008VPsKSPy"
+);
+
+// for performance reasons we should avoid calling the "loadStripe" method
+// inside the render method
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
