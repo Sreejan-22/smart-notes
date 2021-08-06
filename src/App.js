dd3f034b1313d65ac9761566b2f92a80cedc660e
Sreejan <sreejan147@gmail.com>
2021-08-06 23:50:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
-import Modal from "./Modal";
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <div className="app backgroundClass">
-      <h1>React Modal</h1>
-      <br />
-      <div>
-        <button style={{ width: "180px" }} onClick={() => setIsOpen(true)}>
-          Click to open the Modal
-        </button>
-      </div>
-      <p style={{ margin: "3rem" }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
-        non sunt cum mollitia est inventore at, nisi nobis perferendis ratione
-        cumque ad unde, maxime suscipit incidunt corporis tempore quaerat.
-        Laborum.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Perspiciatis, non sunt cum mollitia est inventore at, nisi nobis
-        perferendis ratione cumque ad unde, maxime suscipit incidunt corporis
-        tempore quaerat. Laborum.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Perspiciatis, non sunt cum mollitia est inventore at,
-        nisi nobis perferendis ratione cumque ad unde, maxime suscipit incidunt
-        corporis tempore quaerat. Laborum.
-      </p>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-        }}
-      >
-        <img
-          src="https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          alt=""
-        />
-        <img
-          src="https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          alt=""
-        />
-      </div>
-      {isOpen && <Modal setIsOpen={setIsOpen} />}
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 };
