d61ba7d9d5214d1c1a9c14e5fcd25361ee610dab
Sreejan <sreejan147@gmail.com>
2021-08-09 01:38:54 +0530

    feat: added AppBar, Avatars | style: applied masonry layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,4 @@
+import { useHistory, useLocation } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
 import Drawer from "@material-ui/core/Drawer";
 import Typography from "@material-ui/core/Typography";
@@ -7,7 +8,9 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import SubjectOutlinedIcon from "@material-ui/icons/SubjectOutlined";
 import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
-import { useHistory, useLocation } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { format } from "date-fns";
 
 const drawerWidth = 240;
 
@@ -33,6 +36,21 @@ const useStyles = makeStyles((theme) => {
     title: {
       padding: theme.spacing(2),
     },
+    appbar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+    // we are using mixins here
+    // it's a collection of styles used by one of Material UI's components
+    // basically the styles including the height of the Toolbar component will be assigned to the
+    // component with className "spaceBelowToolbar"
+    spaceBelowToolbar: theme.mixins.toolbar,
+    date: {
+      flexGrow: 1,
+      color: "gray",
+    },
+    user: {
+      color: "gray",
+    },
   };
 });
 
@@ -57,6 +75,14 @@ const Layout = ({ children }) => {
   return (
     <div className={classes.root}>
       {/* app bar */}
+      <AppBar color="inherit" className={classes.appbar} elevation={1}>
+        <Toolbar>
+          <Typography className={classes.date}>
+            {format(new Date(), "do MMMM y")}
+          </Typography>
+          <Typography className={classes.user}>Sreejan</Typography>
+        </Toolbar>
+      </AppBar>
 
       {/* side drawer */}
       <Drawer
@@ -89,7 +115,11 @@ const Layout = ({ children }) => {
         </List>
       </Drawer>
 
-      <div className={classes.page}>{children}</div>
+      <div className={classes.page}>
+        {/* craete some space above using a div to prevent the notes from getting overlapped; height of this div needs to be equal to the height of the toolbar */}
+        <div className={classes.spaceBelowToolbar}></div>
+        {children}
+      </div>
     </div>
   );
 };
